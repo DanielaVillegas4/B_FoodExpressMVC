3c483056fa867e3823fcda3c3035b60298686a1e
melola <melola@educa.madrid.org>
2025-11-17 19:22:03 +0100

    Montada la conexión para listar restaurantes.

--- src/main/java/es/daw/foodexpressmvc/service/RestaurantsService.java
+++ src/main/java/es/daw/foodexpressmvc/service/RestaurantsService.java
@@ -1,10 +1,12 @@
 package es.daw.foodexpressmvc.service;
 
 import es.daw.foodexpressmvc.dto.RestaurantDTO;
+import es.daw.foodexpressmvc.exception.ConnectionApiRestException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -23,13 +25,15 @@ public class RestaurantsService {
                     .uri("/restaurants")
                     .retrieve()
                     .bodyToMono(RestaurantDTO[].class)
-                    .block();
+                    .block(); //asíncrono
         }catch (Exception e){
             // Pendiente crear excepción propia
             // Pendiente crear Globla ExceptionHancler: que lea la exceión y redirija a api-error
             //
             throw new ConnectionApiRestException("Could not connect to FoodExpress API");
         }
+
+        return Arrays.asList(restaurants);
 
 
 
